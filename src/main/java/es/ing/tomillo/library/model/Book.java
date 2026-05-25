984861aba10944b7e134aca1a114d335d1473db8
Jorge <Jorgeesquinas09@gmail.com>
2026-05-25 11:55:10 +0200

    Rama-de-Jorge-Marcos-Abel-Ronal

--- src/main/java/es/ing/tomillo/library/model/Book.java
+++ src/main/java/es/ing/tomillo/library/model/Book.java
@@ -8,14 +8,29 @@ public class Book {
     // - autor (String)
     // - isbn (String)
     // - disponible (boolean)
+        private String titulo;
+        private String autor;
+        private String isbn;
+        private boolean disponible;
 
     // TODO: Implementar constructor según el ejercicio 1
-    public Book() {
+    public Book(String titulo,String autor,String isbn) {
+        this.titulo=titulo;
+        this.autor=autor;
+        this.isbn=isbn;
+        this.disponible=true;
     }
 
     // TODO: Implementar getters y setters según el ejercicio 1
+        public String getTitulo() {
+        return titulo;
+        return autor;
+        return isbn;
+        return disponible;
+        }
 
     // TODO: Implementar método toString según el ejercicio 1
+
 
     // TODO: Implementar método equals para comparar libros por ISBN
 
